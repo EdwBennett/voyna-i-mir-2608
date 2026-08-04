acc2cf062db1f4beb4e71de768359311238bac18
Ed Bennett <edw.bennett@gmail.com>
2026-08-04 06:21:19 -0500

    improve test print, use f string

--- src/db/test_sentence_pairs.py
+++ src/db/test_sentence_pairs.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     # Select sentences whose .id is 1, 3, 5, 6, 7, or 8
     SentencePairs("50_russian_english_ipa.json") \
         .filter(parse_page_list("1,3,5-8")) \
-        .execute(lambda s: print(s.id, s.en))
+        .execute(lambda s: print(f"{s.id}\n{s.en}\n{s.ru}"))
+    print()
 
     # As a list
     subset = (
