21419422f9b593a24be1e024b50c87982eb47f49
Ed Bennett <edw.bennett@gmail.com>
2026-08-03 22:07:58 -0500

    cli.py, new __main__.py, how to run.txt

--- src/voyna-i-mir-2608/__main__.py
+++ src/voyna-i-mir-2608/__main__.py
@@ -1,5 +1,5 @@
-def main():
-    print("Hello, World!")
+from .cli import main
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
+    
---
+++ src/voyna-i-mir-2608/cli.py
@@ -0,0 +1,4 @@
+def main() -> int:
+    print("Hello from voyna_i_mir_2608")
+    return 0
+    
