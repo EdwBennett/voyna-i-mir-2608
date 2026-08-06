e95af9b12f642ba61518902403dc1e7ad10e257d
Ed Bennett <edw.bennett@gmail.com>
2026-08-05 21:26:50 -0500

    add clause choice

--- src/voyna_i_mir_2608/play/play_en.py
+++ src/voyna_i_mir_2608/play/play_en.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from typing import Optional
+import re
 
 from collections.abc import Callable
 from voyna_i_mir_2608.db.sentence_pairs import (
@@ -10,12 +12,18 @@ from voyna_i_mir_2608.say.say import say
 
 JSON_PATH = Path(__file__).resolve().parent.parent / "db" / "50_russian_english_ipa_words.json"
 
-def play_en(id: int) -> Callable[[], None]:
+def play_en(id: int, clause: Optional[int] = None) -> Callable[[], None]:
 
     def en_fn():
         pairs = SentencePairs(JSON_PATH).filter([id]).to_list()
         pair = pairs[0]
         print (f"\n{pair.en}")
-        say(lang = "en", text=pair.en)
+        if clause is None:
+            en_say: str = pair.en
+            print (f"{pair.words}")
+        else:
+            en_say = re.split(r'[,.;-]+', pair.words)[clause-1].strip()
+            print (f"{en_say}")
+        say(lang = "en", text=en_say)
 
     return en_fn
--- src/voyna_i_mir_2608/play/play_ru.py
+++ src/voyna_i_mir_2608/play/play_ru.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from typing import Optional
+import re
 
 from collections.abc import Callable
 from voyna_i_mir_2608.db.sentence_pairs import (
@@ -10,15 +12,18 @@ from voyna_i_mir_2608.say.say import say
 
 JSON_PATH = Path(__file__).resolve().parent.parent / "db" / "50_russian_english_ipa_words.json"
 
-def play_ru(id: int) -> Callable[[], None]:
+def play_ru(id: int, clause: Optional[int] = None) -> Callable[[], None]:
 
     def ru_fn():
         pairs = SentencePairs(JSON_PATH).filter([id]).to_list()
         pair = pairs[0]
-        say(lang = "ru", text=pair.ru)
-        print (f"{pair.ru}")
-        print (f"{pair.ipa}")
-        print (f"{pair.words}\n")
-
+        if clause is None:
+            ru_say: str = pair.ru
+            print (f"{pair.ru}")
+        else:
+            ru_say = re.split(r'[,.;-]+', pair.ru)[clause-1].strip()
+            print (f"{ru_say}")
+        print (f"{pair.ipa}\n")
+        say(lang = "ru", text=ru_say)
 
     return ru_fn
